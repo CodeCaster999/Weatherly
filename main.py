e5b68e1045b3be9be7d45be0e916a27f3f1624db
Matthew Lancaster <lancastermatthew0702@gmail.com>
2026-02-22 15:14:51 -0500

    Adding basic GUI components for Weatherly

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ class WeatherApp(QWidget):
     def __init__(self):
         # Initialize the base QWidget
         super().__init__()
+        # Label prompting the user to enter a city name
+        self.city_name = QLabel("Enter city name:", self)
+
+        # Text input for the user to enter a city name
+        self.city_input = QLineEdit(self)
+
+        # Button to trigger weather data retrieval
+        self.display_weather_button = QPushButton("Display Weather", self)
+        
+        # Weatehr display elements
+        self.temperature_label = QLabel("70*F", self)
+        self.emoji_label = QLabel(": )", self)
+        self.description_label = QLabel("Sunny", self)
+        
         
 if __name__ == "__main__":
     # Create the application (handles GUI events)
